e45b41cd8cb1fed5f0c80ea46ec1cff2edea7f99
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-09-16 19:02:40 +0530

    navbar component done

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi qui sequi alias laudantium nostrum quidem debitis voluptatum eos tempore deserunt exercitationem quo, consequatur id ad vel aut, sunt unde ducimus magnam sint blanditiis repudiandae. At aliquam laboriosam quasi hic commodi reprehenderit atque, rem temporibus repellat, autem fuga officiis maxime?
+      </p>
+    </section>
   )
 }
 
